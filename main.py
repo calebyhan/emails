e2a56c0eea69ae1eeb29067ce3be3022c64203fc
Shadowfax1162 <calebhantech@gmail.com>
2020-03-22 21:20:54 -0400

    Got a good start.

---
+++ main.py
@@ -0,0 +1,46 @@
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+smtp_server = 'smtp.gmail.com'
+port = 465
+
+sender = 'testemailcyh@gmail.com'
+password = input('Enter your password here: ')
+receiver = 'calebhantech@gmail.com'
+
+message = MIMEMultipart('alternative')
+message['Subject'] = 'Multipart Test'
+message['From'] = sender
+message['To'] = receiver
+
+text = """\
+Hi, 
+How are you?
+fsdfijsidfjsodi
+www.gmail.com
+"""
+
+html = """\
+<html>
+    <body>
+        <p>Hi, <br>
+            How are you?<br>
+            <a href="https://www.google.com/">Google</a>
+            has many great info.
+        </p>
+    </body>
+</html>
+"""
+
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+message.attach(part1)
+message.attach(part2)
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender, password)
+    server.sendmail(sender, receiver, message.as_string())
